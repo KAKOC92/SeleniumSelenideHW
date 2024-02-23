5e754cb96cad91c6cbe7abf5e405a95ca45740b0
Бабкин Алексей <babkin.92@mail.ru>
2024-02-24 01:08:19 +0300

    Tests added

---
+++ src/test/java/AppOrderNegativeTest.java
@@ -0,0 +1,76 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class AppOrderTestNegativeTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDowd() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void SendingFormTestWithInvalidName() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Alexey Babkin");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79102436802");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub" )).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text);
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void SendingFormTestWithoutName() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79102436802");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub" )).getText();
+        assertEquals("Поле обязательно для заполнения", text);
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void SendingFormTestWithOutPhoneNumber() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей Бабкин");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub" )).getText();
+        assertEquals("Поле обязательно для заполнения", text);
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void SendingFormTestWithInvalidPhoneNumber() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей Бабкин");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("Phone number");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub" )).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text);
+        Thread.sleep(5000);
+    }
+}
---
+++ src/test/java/AppOrderTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class AppOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDowd() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void SendingFormTest() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Алексей Бабкин");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79102436802");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+        Thread.sleep(5000);
+    }
+}
